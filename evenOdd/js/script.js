8ba168c1a7d7b8678e9f7d981d9e4274e3ee889d
aalexebr <alex.ebr@icloud.com>
2023-06-20 16:05:53 +0200

    ADD even condition validation

--- evenOdd/js/script.js
+++ evenOdd/js/script.js
@@ -1,6 +1,6 @@
 const userChoice = prompt('even or odd')
 const userNumber =parseInt(prompt('number between 1 and 5'))
-
+console.log('user number', userNumber) 
 //  computer function number
 let min = 1
 let max = 5
@@ -8,7 +8,7 @@ function getRndPcNumber(min, max) {
     return Math.floor(Math.random() * (5 - 1 + 1) ) + 1;
   }
 let pcNumber = getRndPcNumber(min, max)
-console.log(pcNumber) 
+console.log('pc number',pcNumber) 
 
 // funtcion of sum userNumber and pcNumber
 
@@ -17,7 +17,19 @@ function userAndPcSum(userNumber,pcNumber){
 }
 let userAndPcSumResult = userAndPcSum(userNumber,pcNumber)
 
-console.log(userAndPcSumResult) 
+console.log('sum of pc and user',userAndPcSumResult) 
 
 // even and odd conditions
-function even ()
+function even (userAndPcSumResult){
+    if (userAndPcSumResult % 2 == 0){
+        return true
+    }
+    return false
+}
+let evenValidation = even(userAndPcSumResult)
+if (evenValidation == true){
+    console.log('even')
+}
+else if (evenValidation == false){
+    console.log('false')
+}
