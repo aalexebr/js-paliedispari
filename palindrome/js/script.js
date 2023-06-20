30e36b9548a4b2786d6d4b07c5ac3ce322e898a4
aalexebr <alex.ebr@icloud.com>
2023-06-20 17:07:11 +0200

    palindrome complete

--- palindrome/js/script.js
+++ palindrome/js/script.js
@@ -1,2 +1,17 @@
 const userWord = prompt('insert word')
+console.log('user word', userWord)
+// let reverseUserWord;
+// let splitString = userWord.split('');
 
+function reverse(){
+    return userWord.split("").reverse().join("");
+}
+reverseUserWord = reverse();
+console.log('reversed word', reverseUserWord);
+
+if (userWord == reverseUserWord){
+    console.log('the word is a palindrome')
+}
+else{
+    console.log('the word is NOT a palindrome')
+};
