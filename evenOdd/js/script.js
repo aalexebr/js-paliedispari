62a59aeff8687bffb2f38da33ff21421a5908223
aalexebr <alex.ebr@icloud.com>
2023-06-20 16:55:20 +0200

    ADD - do while conditions for promt

--- evenOdd/js/script.js
+++ evenOdd/js/script.js
@@ -1,27 +1,34 @@
 
-const userChoice = prompt('even or odd')
-console.log('user choise ', userChoice)
-while (userChoice !== 'odd' || userChoice !== 'even'){
+let userChoice;
+do{
     userChoice = prompt('even or odd')
-}
-const userNumber =parseInt(prompt('number between 1 and 5'))
+ }
+while (userChoice !== 'odd' && userChoice !== 'even');
+console.log('user choise ', userChoice)
+
+let userNumber;
+do{
+    userNumber = parseInt(prompt('number between 1 and 5'))
+ }
+while (userNumber > 5 );
+
 console.log('user number', userNumber) 
 
 //  computer function number
-let min = 1
-let max = 5
+let min = 1;
+let max = 5;
 function getRndPcNumber(min, max) {
-    return Math.floor(Math.random() * (5 - 1 + 1) ) + 1;
-  }
-let pcNumber = getRndPcNumber(min, max)
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  };
+let pcNumber = getRndPcNumber(min, max);
 console.log('pc number',pcNumber) 
 
 // funtcion of sum userNumber and pcNumber
 
 function userAndPcSum(userNumber,pcNumber){
     return userNumber + pcNumber
-}
-let userAndPcSumResult = userAndPcSum(userNumber,pcNumber)
+};
+let userAndPcSumResult = userAndPcSum(userNumber,pcNumber);
 
 console.log('sum of pc and user',userAndPcSumResult) 
 
@@ -31,7 +38,7 @@ function even (userAndPcSumResult){
         return true
     }
     return false
-}
+};
 let evenValidation = even(userAndPcSumResult)
 if (evenValidation == true){
     console.log('then number is even')
@@ -50,4 +57,4 @@ else if (evenValidation == false){
     else{
         console.log('user chose odd and lost')
     }
-}
+};
